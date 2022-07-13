4954712a07e9a9f71226e799b782f01f4aca4b0c
rkrakibhasan680 <rkrakibhasan680@gmail.com>
2022-07-13 21:14:43 +0600

    create a refucer

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+// redux has 4 step 
+// 1. state
+// 2.dispatch action
+// reducer
+// store ==> getState(),dispatch(),subscribe()
+
+
+
 // create INCREMENT and DECREMENT 
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
 const ADD_USER = 'ADD_USER';
 
-// state
+// 1.state
 const initialCountState = {
-    count: 0
+    count: 0,
+    users: [{ name: "rakib" }]
 }
 const intialUsersState = {
     users: [{ name: "rakib" }]
 }
 
-// Action ===> object-type,payload 
+//2. Action ===> object-type,payload 
 const incrementCounter = () => {
     return { type: INCREMENT }
 }
@@ -19,8 +28,26 @@ const decrementCounter = () => {
     return { type: DECREMENT }
 }
 const addUser = () => {
-return {
-    type:ADD_USER,
-    payload:{name:"rakib Mia"}
+    return {
+        type: ADD_USER,
+        payload: { name: "rakib Mia" }
+    }
 }
+// reducer create 
+const counterReducer = (state = initialCountState, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count + 1
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count - 1
+            }
+
+        default:
+            state
+    }
 }
